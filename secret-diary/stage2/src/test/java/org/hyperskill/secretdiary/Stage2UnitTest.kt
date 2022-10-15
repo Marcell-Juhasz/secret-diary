540c097482a69382e5bff67a0d5c323d2bc6339d
marcell <marcell.void@gmail.com>
2022-10-15 20:03:58 +0200

    Date and time check extended for issue #22

--- secret-diary/stage2/src/test/java/org/hyperskill/secretdiary/Stage2UnitTest.kt
+++ secret-diary/stage2/src/test/java/org/hyperskill/secretdiary/Stage2UnitTest.kt
@@ -4,6 +4,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toKotlinInstant
+import kotlinx.datetime.toLocalDateTime
 import org.hyperskill.secretdiary.internals.AbstractUnitTest
 import org.hyperskill.secretdiary.internals.CustomClockSystemShadow
 import org.junit.Assert.assertEquals
@@ -15,7 +18,7 @@ import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowToast
 import java.text.SimpleDateFormat
 import java.time.Duration
-import java.util.Locale
+import java.util.*
 
 
 @RunWith(RobolectricTestRunner::class)
@@ -86,27 +89,52 @@ class Stage2UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
             etNewWriting.setText(sampleInputText1)
             val instant1 = Clock.System.now()
             val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-            val dateText1 = simpleDateFormat.format(instant1.toEpochMilliseconds())
+            val dateTimeText1 = simpleDateFormat.format(instant1.toEpochMilliseconds())
+            val dateTime1 = simpleDateFormat.parse(dateTimeText1)?.toInstant()?.toKotlinInstant()
+                ?.toLocalDateTime(TimeZone.currentSystemDefault())
             btnSave.clickAndRun()
 
             val expectedOutput1 = """
-            $dateText1
+            $dateTimeText1
             $sampleInputText1
         """.trimIndent()
             val userOutput1 = tvDiary.text.toString()
 
-            val messageWrongDate1 =
-                "Make sure you are using UTC time zone and the date/time has the following format with the correct values: \"yyyy-MM-dd HH:mm:ss\""
-            assertTrue(messageWrongDate1, userOutput1.contains(dateText1))
-
+            // check number of lines
             val linesExpected1 = expectedOutput1.split("\n").size
             val linesUser1 = userOutput1.split("\n").size
             val messageWrongNumberOfLines1 =
                 "The diary should contain $linesExpected1 lines, but $linesUser1 found"
             assertTrue(messageWrongNumberOfLines1, linesExpected1 == linesUser1)
 
+            // check date and time format
+            val dateTimeRegex = Regex("\\d{4}-\\d{2}-\\d{2}[ ]\\d{2}:\\d{2}:\\d{2}")
+            val userDateTimeText1 = userOutput1.substringBefore("\n")
+            val userDateTime1 =
+                simpleDateFormat.parse(userDateTimeText1)?.toInstant()?.toKotlinInstant()
+                    ?.toLocalDateTime(TimeZone.currentSystemDefault())
+            val messageWrongDateTimeFormat =
+                "Date and time should be on the first line in the following format: \"yyyy-MM-dd HH:mm:ss\""
+            assertTrue(messageWrongDateTimeFormat, dateTimeRegex.matches(userDateTimeText1))
+
+            // check date (year, month, day) values
+            val messageWrongYearValue = "Wrong year value!"
+            assertEquals(messageWrongYearValue, dateTime1?.year, userDateTime1?.year)
+
+            val messageWrongMonthValue = "Wrong month value!"
+            assertEquals(messageWrongMonthValue, dateTime1?.monthNumber, userDateTime1?.monthNumber)
+
+            val messageWrongDayValue = "Wrong day value!"
+            assertEquals(messageWrongDayValue, dateTime1?.dayOfMonth, userDateTime1?.dayOfMonth)
+
+
+            // check HOUR matching (Time Zone error)
+            val messageWrongTimeZone = "You should use your local time zone!"
+            assertEquals(messageWrongTimeZone, dateTime1?.hour, userDateTime1?.hour)
+
+
             val messageWrongOutput1 =
-                "The first line should be the date and time, the second line is the saved text"
+                "The first line should be the date and time and the second line is the saved text"
             assertEquals(messageWrongOutput1, expectedOutput1, userOutput1)
 
             val messageEtNotCleared = "EditText should be cleared after each saving"
@@ -120,14 +148,14 @@ class Stage2UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
             val sampleInputText2 = "I had a date with my crush"
             etNewWriting.setText(sampleInputText2)
             val instant2 = Clock.System.now()
-            val dateText2 = simpleDateFormat.format(instant2.toEpochMilliseconds())
+            val dateTimeText2 = simpleDateFormat.format(instant2.toEpochMilliseconds())
             btnSave.clickAndRun()
 
             val expectedOutput2 = """
-            $dateText2
+            $dateTimeText2
             $sampleInputText2
             
-            $dateText1
+            $dateTimeText1
             $sampleInputText1
         """.trimIndent()
             val userOutput2 = tvDiary.text.toString()
@@ -172,7 +200,8 @@ class Stage2UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
 
             val userToastText = ShadowToast.getTextOfLatestToast()
             val savingBlankToastText = "Empty or blank input cannot be saved"
-            val messageWrongToastText = "When trying to save an empty or blank string, the appropriate Toast message should be shown"
+            val messageWrongToastText =
+                "When trying to save an empty or blank string, the appropriate Toast message should be shown"
             assertEquals(messageWrongToastText, savingBlankToastText, userToastText)
 
             val diaryTextAfterSaveBlank = tvDiary.text
